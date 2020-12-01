1a4b2ad0013cbb8dd866e1599e2ef0454d19ba81
EmmanuelWiredu <58808378+EmmanuelWiredu@users.noreply.github.com>
2020-12-01 08:49:45 +0530

    completed
    both login and registration on online database done

---
+++ locationBasedTeasureHunt_login_registration_complete/Hunt/app/src/main/java/com/example/hunt/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.hunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText UsernameEt, PasswordEt;
+    Button btn_reg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UsernameEt = (EditText)findViewById(R.id.etUserName);
+        PasswordEt = (EditText)findViewById(R.id.etPassword);
+        btn_reg = (Button)findViewById(R.id.btn_reg);
+        btn_reg.setOnClickListener(new View.OnClickListener(){
+       @Override
+       public void onClick(View v){
+           OpenReg();
+       }
+    });
+    }
+
+    public void OnLogin(View view) {
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
+    }
+
+
+    public void OpenReg() {
+        Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ locationBasedTeasureHunt_login_registration_complete/Hunt/app/src/main/java/com/example/hunt/Register.java
@@ -0,0 +1,36 @@
+package com.example.hunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+EditText name, surname, age, username, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        name=(EditText)findViewById(R.id.et_name);
+        surname=(EditText)findViewById(R.id.et_surname);
+        age=(EditText)findViewById(R.id.et_age);
+        username=(EditText)findViewById(R.id.et_username);
+        password=(EditText)findViewById(R.id.et_password);
+    }
+    public void OnReg(View view) {
+        String str_name = name.getText().toString();
+        String str_surname = surname.getText().toString();
+        String str_age = age.getText().toString();
+        String str_username = username.getText().toString();
+        String str_password = password.getText().toString();
+        String type = "register";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, str_name, str_surname , str_age, str_username, str_password);
+    }
+
+}
